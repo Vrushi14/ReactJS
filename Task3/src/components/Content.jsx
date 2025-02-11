94c31f7c4026df5e6417c6c99f9ab2c342fe4a83
unknown <vrushi1214@example.com>
2025-02-11 21:50:16 +0530

    2203031080222 Assignment33

---
+++ Task3/src/components/Content.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Content = () => {
+  return (
+    <>
+    <h2>Main Content</h2>
+    <p>I learned ReactJs today.</p>
+    </>
+  )
+}
+
+export default Content
---
+++ Task3/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+   <>
+   <h1>Welcome to My Portfolio</h1>
+   <a href="#home">Home</a>
+   <a href="#about">About</a>
+   <a href="#contact">Contact</a>
+   </>
+  )
+}
+
+export default Header
