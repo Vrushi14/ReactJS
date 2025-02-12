5eb7a56d985ec90130344f1be9cb24d33c7d0fec
unknown <vrushi1214@example.com>
2025-02-12 09:41:02 +0530

    2203031080222 Assignment36

---
+++ Task6/src/App.jsx
@@ -0,0 +1,15 @@
+import react from 'react'
+import Info from './components/Info'
+import Greeting from './components/Greeting'
+
+function App() {
+    return (
+        <div>
+            <h1>App Component</h1>
+            <Greeting></Greeting>
+            <Info></Info>
+        </div>
+    )
+}
+
+export default App
