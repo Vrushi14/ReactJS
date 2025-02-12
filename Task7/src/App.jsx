691890d81ebf27d6af843960b16e2c92547984f1
unknown <vrushi1214@example.com>
2025-02-12 11:38:16 +0530

    2203031080222 Assignment36

---
+++ Task7/src/App.jsx
@@ -0,0 +1,14 @@
+import react from 'react';
+import UserList from './components/UserList';
+import ProductList from './components/ProductList';
+
+const App = () => {
+    return (
+        <>
+            <UserList />
+            <ProductList />
+        </>
+    );
+}
+
+export default App;
